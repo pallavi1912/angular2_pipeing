e4072fc0b073ce794f7f3c40287efbe7d40789e8
Pallavi Verma <vermapallavi93@gmail.com>
2017-02-14 19:26:13 -0800

    capitalize pipeing issue solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div>
+      <p>My name is {{name | captalize}}</p>
       <ul>
         <li *ngFor="let num of numbers">
           {{ num | ordinal }}
@@ -13,7 +14,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  numbers:any;
+ name:any;
  constructor(){
+  this.name = "pallavi verma";
   this.numbers=[1,2,3,4,5,6,7,8,9,12,11113,123];
  }
 }
